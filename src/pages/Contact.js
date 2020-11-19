ac5ccea56fbb2938e5dba08a8935fadd0b956fb2
lukas <wski.lukas@gmail.com>
2020-11-19 11:06:09 +0100

    bugs in deploy

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   Fade, Col, Row, Button,
 } from 'react-bootstrap';
-import { StyledContainer, StyledCol } from './StyledContact';
+import { StyledContainer, StyledCol } from './styledContact';
 import Footer from '../components/Footer';
 
 export default () => {
